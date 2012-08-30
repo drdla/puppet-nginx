6c603f884b470d9d821b5e00acb8e9d97a52fd4f
Dominik Rodler <dominik.rodler@googlemail.com>
2012-08-30 23:44:19 +0200

    Just fixes for *MY* puppet dependencies

--- manifests/package/redhat.pp
+++ manifests/package/redhat.pp
@@ -14,17 +14,20 @@
 #
 # This class file is not called directly
 class nginx::package::redhat {
-  $redhat_packages = ['nginx', 'GeoIP', 'gd', 'libXpm', 'libxslt']
+  $redhat_packages  = ['nginx', 'GeoIP', 'gd', 'libXpm']
+  # TODO: already declared in my rvm module and can´t be redeclared
+  # $redhat_packages = ['nginx', 'GeoIP', 'gd', 'libXpm', 'libxslt']
+  $os_type          = downcase($::operatingsystem)
 
   yumrepo { "nginx-release":
-    baseurl  => 'http://nginx.org/packages/OS/OSRELEASE/$basearch/'
-    descr    => 'nginx repo',
-    enabled  => '1',
-    gpgcheck => '0',
+    baseurl   => "http://nginx.org/packages/${os_type}/${::lsbmajdistrelease}/\$basearch/",
+    descr     => 'nginx repo',
+    enabled   => '1',
+    gpgcheck  => '0',
   }
 
   package { $redhat_packages:
-    ensure  => present,
-    require => Yumrepo['nginx-release'],
+    ensure    => present,
+    require   => Yumrepo['nginx-release'],
   }
 }
